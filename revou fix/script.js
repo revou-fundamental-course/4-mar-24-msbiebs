0faba67846689ff0a0075e788009c7e8557c2bda
msbiebs <162128888+msbiebs@users.noreply.github.com>
2024-03-13 14:46:18 +0800

    deployment

--- revou fix/script.js
+++ revou fix/script.js
@@ -12,6 +12,7 @@ var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
 
+
 function calculate(){
  
   if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
@@ -37,13 +38,13 @@ function countBmi(){
       
   var result = '';
   if(bmi<18.5){
-    result = 'Kekurangan Berat Badan';
+    result = 'Kekurangan Berat Badan, kondisi ini terjadi ketika seseorang memiliki berat badan di bawah rentang normal pada orang seusianya. Kondisi ini perlu segera ditangani dengan tepat.';
      }else if(18.5<=bmi&&bmi<=24.9){
-    result = 'Normal (ideal)';
+    result = 'Normal (ideal). Tetap jaga pola makan yang sehat dan jangan lupa untuk tetap berolahraga.';
      }else if(25<=bmi&&bmi<=29.9){
-    result = 'Kelebihan Berat Badan';
-     }else if(35<=bmi){
-    result = 'Kegemukan';
+    result = 'Kelebihan Berat Badan, kondisi ini melebihi berat badan normal pada umumnya. Tingkatkan konsumsi sayuran dan buah-buahan serta rutin berolahraga';
+     }else if(30<=bmi){
+    result = 'Kegemukan (obesitas), yaitu kondisi kronis yang ditandai dengan penumpukan lemak dalam tubuh. Anda memiliki kandungan lemak di atas normal sehingga berimbas pada risiko penyakit yang lebih serius bahkan mengancam jiwa.';
      }
 
 
